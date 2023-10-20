2d1c8923c800ea35b3499e95bfffbf6e0eca68f5
Peterson Moro <peterson.airesmoro@hotmail.com>
2023-10-19 23:01:32 -0300

    Primeira tentativa de publicação no Railway

---
+++ src/main/java/me/dio/domain/model/BaseItem.java
@@ -0,0 +1,39 @@
+package me.dio.domain.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass //Possibilita extenções
+public abstract class BaseItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String icon;
+    private String desciprtion;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getDesciprtion() {
+        return desciprtion;
+    }
+
+    public void setDesciprtion(String desciprtion) {
+        this.desciprtion = desciprtion;
+    }
+}
---
+++ src/main/java/me/dio/service/impl/UserServiceImpl.java
@@ -0,0 +1,32 @@
+package me.dio.service.impl;
+
+import me.dio.domain.model.User;
+import me.dio.domain.repository.UserRepository;
+import me.dio.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service //Camada de negocio
+public class UserServiceImpl implements UserService {
+
+    //ToDo AutoWired
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public User create(User userToCreate) {
+        if(userToCreate.getId() != null && userRepository.existsById(userToCreate.getId())){
+            throw new IllegalArgumentException("This User ID already exist!");
+        }
+        return userRepository.save(userToCreate);
+    }
+}
